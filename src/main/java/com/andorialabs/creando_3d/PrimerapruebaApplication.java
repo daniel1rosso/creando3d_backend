67b8d72e3804043663f096df2205862ffa78195e
Damian Pramparo <dpramparo@startuniversity.net>
2021-08-03 11:22:27 -0300

    Cambios en configuraciones

---
+++ src/main/java/com/andorialabs/creando_3d/PrimerapruebaApplication.java
@@ -0,0 +1,13 @@
+package com.andorialabs.creando_3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerapruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerapruebaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/andorialabs/creando_3d/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.andorialabs.creando_3d;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PrimerapruebaApplication.class);
+	}
+
+}
